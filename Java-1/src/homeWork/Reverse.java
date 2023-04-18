b96c35365e2e09d2db3c5d0bd03e0a966622da17
azama <a.kurmanov@csi.kz>
2023-04-18 20:49:48 +0600

    HomeWork-1

--- Java-1/src/homeWork/Reverse.java
+++ Java-1/src/homeWork/Reverse.java
@@ -9,6 +9,7 @@ public class Reverse {
     public static void reverseString(char[] s) {
         int initIndx = 0;
         int afterIndx = s.length-1;
+        //---Changing the data with indexes within one array
         while(initIndx <= afterIndx){
             char temp = s[initIndx];
             s[initIndx] = s[afterIndx];
